b4524b19db1bf0c4b9b6ec48cc95e3043d42f619
Kevin Duong <kdy480@yahoo.com>
2016-11-26 19:01:31 -0500

    w/e

---
+++ app/src/main/java/com/example/swiftly/swiftly/Adapter.java
@@ -0,0 +1,8 @@
+package com.example.swiftly.swiftly;
+
+/**
+ * Created by Kevin on 11/25/16.
+ */
+
+public class Adapter {
+}
---
+++ app/src/main/java/com/example/swiftly/swiftly/TouchHandler.java
@@ -0,0 +1,8 @@
+package com.example.swiftly.swiftly;
+
+/**
+ * Created by Kevin on 11/26/16.
+ */
+
+public class TouchHandler {
+}
