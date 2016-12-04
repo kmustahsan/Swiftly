bc5af331ddd40f32c444be251ce8b6b11d162912
Kevin Duong <kdy480@yahoo.com>
2016-12-04 15:47:32 -0500

    toString, implement Serializable

--- app/src/main/java/com/example/swiftly/swiftly/Receipt.java
+++ app/src/main/java/com/example/swiftly/swiftly/Receipt.java
@@ -1,14 +1,18 @@
 package com.example.swiftly.swiftly;
 
+import android.os.Parcelable;
+
+import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
 /**
  * Created by Kevin on 11/26/16.
  */
 
-public class Receipt {
+public class Receipt implements Serializable {
     ArrayList<JSONObject> items;
     float subtotal;
     float tax;
@@ -51,5 +55,40 @@ public class Receipt {
 
     public void setTotal(float total) {
         this.total = total;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder str = new StringBuilder();
+        try {
+            for (int i = 0; i < getItems().size(); i++) {
+                JSONObject item = items.get(i);
+                String name = item.get("count") + " " + item.get("name").toString();
+                float cost = Float.parseFloat(item.get("price").toString());
+                cost *= Float.parseFloat(item.get("count").toString());
+                String price = "$" + String.format("%.2f", cost);
+                str.append(name);
+                str.append("                  ");
+                str.append(price);
+                str.append("/n");
+            }
+            str.append("Subtotal:                   ");
+            str.append("$");
+            str.append(String.format("%.2f", subtotal));
+            str.append("/n");
+            str.append("Tax:                        ");
+            str.append("$");
+            str.append(String.format("%.2f", tax));
+            str.append("/n");
+            str.append("Total:                      ");
+            str.append("$");
+            str.append(String.format("%.2f", total));
+
+            return str.toString();
+        }
+        catch (JSONException ex) {
+            ex.printStackTrace();
+        }
+        return super.toString();
     }
 }
